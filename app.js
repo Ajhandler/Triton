a8695ec8d239f36e08a7bf8b9567b73d48e256e4
ajhandler <alexjhandler@gmail.com>
2016-06-20 13:19:51 -0400

    break down functions

--- app.js
+++ app.js
@@ -1,19 +1,23 @@
 $('#submit').on("click",function(e){
 	e.preventDefault();
-	var searchVal = $("#search").val();
-	var req = searchVal.split(' ').join('+')
-	var url = "http://www.omdbapi.com/?s=" + req + "&y=&plot=full&r=json"
-	// $("#content").html("<p>Loading...</p>")
+	$("#content").html("<p>Loading...</p>")
+	loadMovies(grabInfo());
+});
 
+$("#search").on("keyup",function(){
+	$("#content").html("<p>Loading...</p>")
+	loadMovies(grabInfo())
+})
+
+function loadMovies(url){
 	$.ajax({
 		url: url,
 		dataType: 'json',
 		success: function(data){
-			console.log(data.Search)
 			var html;
 			data.Search.map(function(result){
 				html += "<li class='card'>"
-				html+= "<a href='http://www.imdb.com/title/'"+ result.imdbID + ">"
+				html+= "<a href='http://www.imdb.com/title/'"+result.imdbID + ">"
 				html += "<img src =" + result.Poster + ">"
 				html += "</a>"
 				html += "<h3 class='title'>" + result.Title + "</h3>"
@@ -24,5 +28,12 @@ $('#submit').on("click",function(e){
 			$("#content").html(html)
 		}
 	})
-});
+}
 
+function grabInfo(){
+	var url;
+	var searchVal = $("#search").val();
+	var req = searchVal.split(' ').join('+')
+	url = "http://www.omdbapi.com/?s=" + req + "&y=&plot=full&r=json"
+	return url
+}
